bcf870df469f00b0a32fe70b9ec8331db5663e46
hiotareq <62850531+hiotareq@users.noreply.github.com>
2020-10-05 23:40:54 +0300

    upd mycp.c

--- 3rd_week/mycp.c
+++ 3rd_week/mycp.c
@@ -1,184 +1,80 @@
 #include<stdio.h>
 #include<fcntl.h>
-#include<stdlib.h>
-#include<string.h>
 #include<unistd.h>
+#include <stdbool.h>
+#include <getopt.h>
+#include <stdlib.h>
 
 int main( int argc, char* argv[]){
+    if (argc < 3) {perror("Wrong input"); return -1;}
     if ( argc == 3){
-        int fd1 = open( argv[1], O_RDONLY);
-        if ( fd1 < 0)	{ perror("open failed"); return -1;}
-        int fd2 = open( argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0777 );
-        if ( fd2 < 0)	{ perror("open failed"); return -1;}
-        char buffer[10];
+        if (argv[1] == argv[2]){//копирование в самого себя, ничего не происходит
+            return 0;
+        }
+        int fd1 = open( argv[1], O_RDONLY), result;
+        if ( fd1 < 0)	{perror("Open failed"); return -1;}
+        int fd2 = open( argv[2], O_WRONLY | O_CREAT | O_TRUNC , 0777);
+        if ( fd2 < 0)	{perror("Open failed"); return -1;}
+        char buffer[1000];
         ssize_t n;
-        while( ( n = read( fd1, buffer, sizeof(buffer))) != 0){
-            if ( n < 0 ) {
-                perror("read failed");
+        while (true){
+            n = read( fd1, buffer, sizeof( buffer));
+            if ( n < 0){
+                perror("Read failed");
                 return -1;
             }
-            write( fd2, buffer, n);
+            if ( n == 0) break;
+            write(fd2, buffer, n);
         }
-        if ( close( fd1) < 0) { perror("close failed") ; return -1;}
-        if ( close( fd2) < 0) { perror("close failed") ; return -1;}
-        close( fd1);
-        close( fd2);
+        result = close( fd1);
+        if ( result < 0) {perror("Close of the old file failed"); return -1;}
+        result = close(fd2);
+        if ( result < 0) {perror("Close of the new file failed"); return -1;}
+        return 0;
+    }else{//argc>3
+        bool flagv = false, flagi = false, flagf = false;
+        while( true) {
+            struct option longopts[] = {
+                    {"interactive", 0, NULL, 0},
+                    {"force",       0, NULL, 0},
+                    {"verbose",     0, NULL, 0},
+                    {0,             0, 0, 0}
+            };
+            int LongIndex;
+            int c = getopt_long(argc, argv, "ivf", longopts, &LongIndex);
+            if ( c == -1) {
+                break;
+            }
+            switch(c){//short options
+                case 'v':flagv = true;  LongIndex = -1; break;
+                case 'i':flagi = true;  LongIndex = -1; break;
+                case 'f':flagf = true;  LongIndex = -1; break;
+                default: printf("You've entered wrong key!"); break;
+            }
+            if ( LongIndex >= 0){//longoptions
+                switch(LongIndex) {
+                    case 0:
+                        flagi = true;
+                        break;
+                    case 1:
+                        flagf = true;
+                        break;
+                    case 2:
+                        flagv = true;
+                        break;
+                    default:
+                        printf("Something's reaaaly wrong...\n");
+                        break;
+                }
+            }
+        }
+        if ( flagv == true && flagf == true && flagi == true){//all options are set
+
+        }
     }
-    if ( argc == 4){
-        if ( argv[1][0] == '-'){
-            if (strstr(argv[1], "f") != NULL){
-                if ( strstr(argv[1], "v") != NULL){
-                    if ( strstr(argv[1], "i") != NULL ){
-                        printf("mycp: переписать '%s' ?", argv[3]);
-                        char* resp;
-                        scanf("%s",resp);
-                        if (resp == "y"){
-                            int fd1 = open( argv[1], O_RDONLY);
-                            if ( fd1 < 0)	{ perror("open failed"); return -1;}
-                            int fd2 = open( argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0777 );
-                            if ( fd2 < 0)	{ perror("open failed"); return -1;}
-                            char buffer[10];
-                            ssize_t n;
-                            while( ( n = read( fd1, buffer, sizeof(buffer))) != 0){
-                                if ( n < 0 ) {
-                                    perror("read failed");
-                                    return -1;
-                                }
-                                write( fd2, buffer, n);
-                            }
-                            if ( close( fd1) < 0) { perror("close failed") ; return -1;}
-                            if ( close( fd2) < 0) { perror("close failed") ; return -1;}
-                            close( fd1);
-                            close( fd2);
-                            printf(" '%s' -> '%s'  ", argv[2], argv[3]);//fvi
-                        }
-                        else return -1;
-                    }else{
-                        int fd1 = open( argv[1], O_RDONLY);
-                        if ( fd1 < 0)	{ perror("open failed"); return -1;}
-                        int fd2 = open( argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0777 );
-                        if ( fd2 < 0)	{ perror("open failed"); return -1;}
-                        char buffer[10];
-                        ssize_t n;
-                        while( ( n = read( fd1, buffer, sizeof(buffer))) != 0){
-                            if ( n < 0 ) {
-                                perror("read failed");
-                                return -1;
-                            }
-                            write( fd2, buffer, n);
-                        }
-                        if ( close( fd1) < 0) { perror("close failed") ; return -1;}
-                        if ( close( fd2) < 0) { perror("close failed") ; return -1;}
-                        close( fd1);
-                        close( fd2);
-                        printf(" '%s' -> '%s'  ", argv[2], argv[3]);//fv
-                    }
-                }else{
-                    if ( strstr(argv[1], "i") != NULL ){
-                        printf("mycp: переписать '%s' ?", argv[3]);
-                        char* resp;
-                        scanf("%s",resp);
-                        if (resp == "y"){
-                            int fd1 = open( argv[1], O_RDONLY);
-                            if ( fd1 < 0)	{ perror("open failed"); return -1;}
-                            int fd2 = open( argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0777 );
-                            if ( fd2 < 0)	{ perror("open failed"); return -1;}
-                            char buffer[10];
-                            ssize_t n;
-                            while( ( n = read( fd1, buffer, sizeof(buffer))) != 0){
-                                if ( n < 0 ) {
-                                    perror("read failed");
-                                    return -1;
-                                }
-                                write( fd2, buffer, n);
-                            }
-                            if ( close( fd1) < 0) { perror("close failed") ; return -1;}
-                            if ( close( fd2) < 0) { perror("close failed") ; return -1;}
-                            close( fd1);
-                            close( fd2);//fi
-                        }else return -1;
-                    }
-                }
-            }else{
-                if ( strstr(argv[1], "v") != NULL){
-                    if ( strstr(argv[1], "i") != NULL ){
-                        printf("mycp: переписать '%s' ?", argv[3]);
-                        char* resp;
-                        scanf("%s",resp);
-                        if (resp == "y"){
-                            int fd1 = open( argv[1], O_RDONLY);
-                            if ( fd1 < 0)	{ perror("open failed"); return -1;}
-                            int fd2 = open( argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0777 );
-                            if ( fd2 < 0)	{ perror("open failed"); return -1;}
-                            char buffer[10];
-                            ssize_t n;
-                            while( ( n = read( fd1, buffer, sizeof(buffer))) != 0){
-                                if ( n < 0 ) {
-                                    perror("read failed");
-                                    return -1;
-                                }
-                                write( fd2, buffer, n);
-                            }
-                            if ( close( fd1) < 0) { perror("close failed") ; return -1;}
-                            if ( close( fd2) < 0) { perror("close failed") ; return -1;}
-                            close( fd1);
-                            close( fd2);
-                            printf(" '%s' -> '%s'  ", argv[2], argv[3]);//vi
-                        }else{
-                            if ( strstr(argv[1], "i") != NULL ){
-                                printf("mycp: переписать '%s' ?", argv[3]);
-                                char* resp;
-                                scanf("%s",resp);
-                                if (resp == "y"){
-                                    int fd1 = open( argv[1], O_RDONLY);
-                                    if ( fd1 < 0)	{ perror("open failed"); return -1;}
-                                    int fd2 = open( argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0777 );
-                                    if ( fd2 < 0)	{ perror("open failed"); return -1;}
-                                    char buffer[10];
-                                    ssize_t n;
-                                    while( ( n = read( fd1, buffer, sizeof(buffer))) != 0){
-                                        if ( n < 0 ) {
-                                            perror("read failed");
-                                            return -1;
-                                        }
-                                        write( fd2, buffer, n);
-                                    }
-                                    if ( close( fd1) < 0) { perror("close failed") ; return -1;}
-                                    if ( close( fd2) < 0) { perror("close failed") ; return -1;}
-                                    close( fd1);
-                                    close( fd2);
-                                    printf(" '%s' -> '%s'  ", argv[2], argv[3]);//v
-                                } else {perror("failed on input"); return -1;}
-                        }else{
-                                if ( strstr(argv[1], "i") != NULL ){
-                                    printf("mycp: переписать '%s' ?", argv[3]);
-                                    char* resp;
-                                    scanf("%s",resp);
-                                    if (resp == "y"){
-                                        int fd1 = open( argv[1], O_RDONLY);
-                                        if ( fd1 < 0)	{ perror("open failed"); return -1;}
-                                        int fd2 = open( argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0777 );
-                                        if ( fd2 < 0)	{ perror("open failed"); return -1;}
-                                        char buffer[10];
-                                        ssize_t n;
-                                        while( ( n = read( fd1, buffer, sizeof(buffer))) != 0){
-                                            if ( n < 0 ) {
-                                                perror("read failed");
-                                                return -1;
-                                            }
-                                            write( fd2, buffer, n);
-                                        }
-                                        if ( close( fd1) < 0) { perror("close failed") ; return -1;}
-                                        if ( close( fd2) < 0) { perror("close failed") ; return -1;}
-                                        close( fd1);
-                                        close( fd2);//i
-                                    }else return -1;
-                                }else return -1;
-                            }
-                        }
-                    }
-                }
-            }else { perror("read failed"); return -1;}
-        }
+        /*
+         * разобраться с getopt_long и применять полученные опции,
+         * не забыть про случай, когда (argc - number_of_options) < 3 ( это ошибка)
+         */
     return 0;
 }
